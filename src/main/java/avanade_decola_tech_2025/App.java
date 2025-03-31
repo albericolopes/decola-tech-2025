fcf4d151378229096a5935e6c2af94b6f8b527b7
Alberico <alberico@Albericos-MacBook-Pro.local>
2025-03-31 18:28:05 -0300

    test

---
+++ src/main/java/avanade_decola_tech_2025/App.java
@@ -0,0 +1,13 @@
+package avanade_decola_tech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
