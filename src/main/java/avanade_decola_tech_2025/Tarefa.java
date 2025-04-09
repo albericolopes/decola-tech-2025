d57f6be505e9feff1035ab058331ed410bf76886
Alberico <alberico@Albericos-MacBook-Pro.local>
2025-04-09 17:05:10 -0300

    To do list

---
+++ src/main/java/avanade_decola_tech_2025/Tarefa.java
@@ -0,0 +1,28 @@
+package avanade_decola_tech_2025;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private boolean concluida;
+
+    // Getters e setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public boolean isConcluida() { return concluida; }
+    public void setConcluida(boolean concluida) { this.concluida = concluida; }
+}
